37b151cb2e8bd5b3e003e2e89d0f836f8dff92db
ASLant <77436463+KA-ASLant@users.noreply.github.com>
2023-01-16 20:53:19 +0800

    Add files via upload

---
+++ js/config.js
@@ -0,0 +1,24 @@
+/*
+ * @Author: N0ts
+ * @Date: 2021-10-08 14:14:37
+ * @LastEditTime: 2022-10-26 15:56:33
+ * @Description: 配置
+ * @FilePath: /eazy-gitee-note/js/config.js
+ * @Mail：mail@n0ts.cn
+ */
+
+export default {
+    // Gitee 配置
+    gitee: {
+        // 账号
+        owner: "n0ts",
+        // 仓库名称
+        repo: "note",
+        // 分支名
+        sha: "master"
+    },
+    // 后端地址
+    serverBase: "https://api.n0ts.cn/api/gitee",
+    // 主题配置
+    Themes: ["default-light", "default-dark"]
+};
